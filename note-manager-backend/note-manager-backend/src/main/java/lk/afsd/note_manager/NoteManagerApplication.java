da8d4de201c24bbfbf9d01bbc0dc4fd8a7dbff28
Isuru <isurudulakshana012@gmail.com>
2023-04-01 10:15:30 +0530

    first commit

---
+++ note-manager-backend/note-manager-backend/src/main/java/lk/afsd/note_manager/NoteManagerApplication.java
@@ -0,0 +1,13 @@
+package lk.afsd.note_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManagerApplication.class, args);
+	}
+
+}
---
+++ note-manager-backend/note-manager-backend/src/test/java/lk/afsd/note_manager/NoteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.afsd.note_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
